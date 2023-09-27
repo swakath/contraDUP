b29ea463b576dd61561e2b60bbc81d71bced42ab
swakath <sumamakeshwaran@gmail.com>
2023-09-27 20:00:18 +0530

    Transition Update with Main Menu features

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -10,16 +10,44 @@ public class CameraController : MonoBehaviour
     private float lookAhead;
     private float currentPosX;
 
+    private void Awake()
+    {
+        player = null;
+    }
+
+    private void OnEnable()
+    {
+        GameManager.OnObjectInstantiated += HandleObjectInstantiated;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnObjectInstantiated -= HandleObjectInstantiated;
+    }
+
+    private void HandleObjectInstantiated(GameObject instantiatedObject)
+    {
+        if (instantiatedObject.tag == "Player")
+        {
+            Debug.Log("[Camera] : I Received the player ");
+            player = instantiatedObject.GetComponent<Transform>();
+        }
+    }
+
     private void Update()
     {
         //Player Camera
-        transform.position = new Vector3(player.position.x + lookAhead,transform.position.y, transform.position.z);
-        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);  
-
+        if (player != null)
+        {
+            transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+            lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+        }
     }
 
     public void MoveToNewScreen(Transform _newRoom)
     {
         currentPosX = _newRoom.position.x;
     }
+
+
 }
