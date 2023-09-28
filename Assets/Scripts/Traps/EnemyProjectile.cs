b12caeef2570a7a0b34146085176b989a08e52e7
swakath <sumamakeshwaran@gmail.com>
2023-09-29 02:51:30 +0530

    fire ball follow sync fix

--- Assets/Scripts/Traps/EnemyProjectile.cs
+++ Assets/Scripts/Traps/EnemyProjectile.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class EnemyProjectile : EnemyDamage      //will damage the player everytime it touches 
 {
-    [SerializeField] private float speed;
+    private float speed;
     [SerializeField] private float resetTime;       //to deactivate obj. after certain amt. of time
     private float lifetime;
+
+    private GameObject player;
+    private Vector3 playerPosition;
 
     private Animator anim;
     private BoxCollider2D coll;
@@ -12,11 +15,22 @@ public class EnemyProjectile : EnemyDamage      //will damage the player everyti
     private bool hit;
     private void Awake()
     {
+        speed = 8f;
         anim = GetComponent<Animator>();
         coll = GetComponent<BoxCollider2D>();
     }
     public void ActivateProjectile()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerPosition = (player.transform.position - transform.position).normalized;
+        }
+        else
+        {
+            Debug.LogError("Player not found to fix the position");
+        }
+
         hit = false;
         lifetime = 0;
         gameObject.SetActive(true);    
@@ -28,8 +42,10 @@ public class EnemyProjectile : EnemyDamage      //will damage the player everyti
         if(hit)         //stop projectile from moving further if it hits something.
             return;
 
-        float movementSpeed = speed * Time.deltaTime;
-        transform.Translate(movementSpeed, 0, 0);
+        //float movementSpeed = speed * Time.deltaTime;
+        //transform.Translate(movementSpeed, 0, 0);
+
+        GetComponent<Rigidbody2D>().velocity = playerPosition*speed;
 
         lifetime += Time.deltaTime;
         if(lifetime> resetTime)
